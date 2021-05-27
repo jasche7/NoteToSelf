025cf90974da24c3d2a8c042f86f951c36ec1d8f
Jasche7 <jasonc1172@gmail.com>
2021-05-26 21:38:34 -0700

    Initial commit

---
+++ src/main/java/com/jasche/notetoself/NoteToSelfApplication.java
@@ -0,0 +1,13 @@
+package com.jasche.notetoself;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteToSelfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteToSelfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasche/notetoself/NoteToSelfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasche.notetoself;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteToSelfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
