024e7a657ab6b1ee03e5e2eb844e73be8c58876e
Jasche7 <jasonc1172@gmail.com>
2021-06-08 23:30:20 -0700

    Okta authentication added

--- src/main/java/com/jasche/notetoself/app/src/App.js
+++ src/main/java/com/jasche/notetoself/app/src/App.js
@@ -1,18 +1,21 @@
 import "./App.css";
 import Home from "./Components/Home";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { CookiesProvider } from "react-cookie";
 import NoteList from "./Components/NoteList";
 import NoteEdit from "./Components/NoteEdit";
 
 const App = () => {
   return (
-    <Router>
-      <Switch>
-        <Route path="/" exact={true} component={Home} />
-        <Route path="/notes" exact={true} component={NoteList} />
-        <Route path="/notes/:id" component={NoteEdit} />
-      </Switch>
-    </Router>
+    <CookiesProvider>
+      <Router>
+        <Switch>
+          <Route path="/" exact={true} component={Home} />
+          <Route path="/notes" exact={true} component={NoteList} />
+          <Route path="/note/:id" component={NoteEdit} />
+        </Switch>
+      </Router>
+    </CookiesProvider>
   );
 };
 
--- src/main/java/com/jasche/notetoself/app/src/Components/Home.js
+++ src/main/java/com/jasche/notetoself/app/src/Components/Home.js
@@ -1,19 +1,93 @@
+import { useState, useEffect } from "react";
 import "../App.css";
 import AppNavbar from "./AppNavbar";
 import { Link } from "react-router-dom";
 import { Button, Container } from "reactstrap";
+import { withCookies } from "react-cookie";
 
-const Home = () => {
+const Home = (props) => {
+  const [isLoading, setLoading] = useState(true);
+  const [isAuthenticated, setAuthenticated] = useState(false);
+  const [user, setUser] = useState(undefined);
+  const { cookies } = props;
+  const [csrfToken] = useState(cookies.get("XSRF-TOKEN"));
+
+  useEffect(() => {
+    const checkUser = async () => {
+      await fetch("/api/user", { credentials: "include" })
+        .then(async (response) => {
+          return response.text();
+        })
+        .then((body) => {
+          if (body === "") {
+            setAuthenticated(false);
+          } else {
+            setAuthenticated(true);
+            setUser(JSON.parse(body));
+          }
+        });
+    };
+
+    checkUser();
+  }, []);
+
+  const login = () => {
+    let port = window.location.port ? ":" + window.location.port : "";
+    if (port === ":3000") {
+      port = ":8080";
+    }
+    window.location.href = "//" + window.location.hostname + port + "/private";
+  };
+
+  const logout = () => {
+    fetch("/api/logout", {
+      method: "POST",
+      credentials: "include",
+      headers: { "X-XSRF-TOKEN": csrfToken },
+    })
+      .then((res) => res.json())
+      .then((response) => {
+        window.location.href =
+          response.logoutUrl +
+          "?id_token_hint=" +
+          response.idToken +
+          "&post_logout_redirect_uri=" +
+          window.location.origin;
+        console.log("This is here: " + window.location.href);
+      });
+  };
+
+  const message = user ? (
+    <h2>Welcome, {user.name}!</h2>
+  ) : (
+    <p>Please log in to manage your JUG Tour.</p>
+  );
+
+  const button = isAuthenticated ? (
+    <div>
+      <Button color="link">
+        <Link to="/notes">Manage JUG Tour</Link>
+      </Button>
+      <br />
+      <Button color="link" onClick={logout}>
+        Logout
+      </Button>
+    </div>
+  ) : (
+    <Button color="primary" onClick={login}>
+      Login
+    </Button>
+  );
+
   return (
     <div>
       <AppNavbar />
       <Container fluid>
-        <Button color="link">
-          <Link to="/notes">Manage Notes</Link>
-        </Button>
+        {message}
+        {button}
       </Container>
     </div>
   );
 };
 
-export default Home;
+export default withCookies(Home);
