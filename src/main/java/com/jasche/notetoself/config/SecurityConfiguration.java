8bfe1d8664dea1b666fae70033b7d7753949661e
Jasche7 <jasonc1172@gmail.com>
2021-06-18 15:04:03 -0700

    Added and set up frontend-maven-plugin

--- src/main/java/com/jasche/notetoself/config/SecurityConfiguration.java
+++ src/main/java/com/jasche/notetoself/config/SecurityConfiguration.java
@@ -1,6 +1,7 @@
 package com.jasche.notetoself.config;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -29,6 +30,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
     }
 
     @Bean
+    @Profile("dev")
     public RequestCache refererRequestCache() {
         return new HttpSessionRequestCache() {
             @Override
