1a0a87c5ad1e7fba3f12764d1f13cfaf6dc38f84
Jasche7 <jasonc1172@gmail.com>
2021-07-06 18:54:17 -0700

    Added documentation

--- src/main/java/com/jasche/notetoself/controller/NoteController.java
+++ src/main/java/com/jasche/notetoself/controller/NoteController.java
@@ -28,6 +28,9 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Optional;
 
+/**
+ * Rest controller for http requests related to Notes.
+ */
 @RestController
 @RequestMapping("/api")
 public class NoteController {
@@ -36,17 +39,35 @@ public class NoteController {
     private final NoteRepository noteRepository;
     private final UserRepository userRepository;
 
+    /**
+     * Constructor for NoteController, initializing with repositories.
+     * @param noteRepository    repository for Note objects
+     * @param userRepository    repository for User objects
+     */
     public NoteController(NoteRepository noteRepository, UserRepository userRepository) {
         this.noteRepository = noteRepository;
         this.userRepository = userRepository;
     }
 
+    /**
+     * This mapping to /api/notes for GET requests returns a Collection of Note objects
+     * which share the same user id number as the Principal given as an argument.
+     * @param principal current user
+     * @return  Collection of Note objects
+     */
     @GetMapping("/notes")
     public Collection<Note> groups(Principal principal) {
         return noteRepository.findAllByUserId(principal.getName());
     }
 
 
+    /**
+     * This mapping to /api/note/{id} for GET requests returns a single Note whose
+     * id matches the id given as an argument. If no note is found, HTTP 404 Not Found is
+     * returned instead.
+     * @param id    id number of note to be retrieved
+     * @return  HTTP response with body containing a Note if found, or else an HTTP 404 Not Found
+     */
     @GetMapping("/note/{id}")
     public ResponseEntity<Note> getNote(@PathVariable Long id) {
         Optional<Note> note = noteRepository.findById(id);
@@ -54,6 +75,16 @@ public class NoteController {
                 .orElse(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+    /**
+     * This mapping to /api/note for POST requests creates a Note object which includes
+     * the user details of the principal and with dateCreated set to current time.
+     * Sets the Note's User to current User if User is in UserRepository, otherwise creates
+     * a new User.
+     * @param note  request body including details for the Note to be created
+     * @param principal current user
+     * @return  http response whose body includes the note that was created
+     * @throws URISyntaxException thrown when URI() is given invalid string
+     */
     @PostMapping("/note")
     public ResponseEntity<Note> createNote(@Valid @RequestBody Note note, @AuthenticationPrincipal OAuth2User principal) throws URISyntaxException {
         log.info("Request to create note: {}", note);
@@ -76,6 +107,12 @@ public class NoteController {
                 .body(result);
     }
 
+    /**
+     * This mapping to /api/note/{id} for PUT requests updates an existing Note in
+     * the repository.
+     * @param note  http request whose body contains a Note
+     * @return  http response whose body contains the updated Note
+     */
     @PutMapping("/note/{id}")
     public ResponseEntity<Note> updateNote(@Valid @RequestBody Note note) {
         log.info("Request to update note: {}", note);
@@ -83,6 +120,12 @@ public class NoteController {
         return ResponseEntity.ok().body(result);
     }
 
+    /**
+     * This mapping to /api/note/{id} for DELETE requests deletes a Note from the
+     * repository according to the id given as an argument.
+     * @param id    id of Note to be deleted
+     * @return  http response with status OK
+     */
     @DeleteMapping("/note/{id}")
     public ResponseEntity<Long> deleteGroup(@PathVariable Long id) {
         log.info("Request to delete note: {}", id);
