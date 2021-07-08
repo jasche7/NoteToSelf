82b2c96945f3fa5d5307bd5fa0888d0986de0706
Jasche7 <jasonc1172@gmail.com>
2021-07-08 00:30:40 -0700

    New test for PUT on /api/note/{id}

--- src/test/java/com/jasche/notetoself/NoteControllerTest.java
+++ src/test/java/com/jasche/notetoself/NoteControllerTest.java
@@ -1,5 +1,6 @@
 package com.jasche.notetoself;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.jasche.notetoself.domain.Note;
 import com.jasche.notetoself.domain.User;
 import com.jasche.notetoself.repository.NoteRepository;
@@ -12,6 +13,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.security.Principal;
@@ -19,6 +22,7 @@ import java.time.Instant;
 
 import static org.hamcrest.Matchers.containsString;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -30,8 +34,14 @@ class NoteControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
+    @Autowired
+    private ObjectMapper mapper;
+
     @Mock
     private Principal principal;
+
+    @Mock
+    private OAuth2User oAuth2User;
 
     @Autowired
     private NoteRepository noteRepository;
@@ -39,26 +49,46 @@ class NoteControllerTest {
     @MockBean
     private UserRepository userRepository;
 
+    static Note testNote;
+
     @BeforeAll
     static void init(@Autowired NoteRepository noteRepository){
         User user = new User("1", "testUser");
-        Note note = Note.builder().dateCreated(Instant.now())
+        testNote = Note.builder().dateCreated(Instant.now())
                 .title("Test Note")
                 .text("Test Note Contents")
                 .user(user)
                 .build();
-        noteRepository.save(note);
+        noteRepository.save(testNote);
     }
 
     @Test
     void shouldGetAllNotes() throws Exception {
         Mockito.when(principal.getName()).thenReturn("1");
-        mockMvc.perform(get("/api/notes").principal(principal)).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Test Note")));
+        mockMvc.perform(get("/api/notes")
+                .principal(principal))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Note")));
     }
 
     @Test
     void shouldGetSpecificNote() throws Exception {
-        mockMvc.perform(get("/api/note/1")).andDo(print()).andExpect(status().isOk()).andExpect(content().string(containsString("Test Note")));
+        mockMvc.perform(get("/api/note/1"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("Test Note")));
     }
 
+    @Test
+    void shouldUpdateExistingNote() throws Exception {
+        Note newNote = testNote;
+        newNote.setText("New Test Note");
+        mockMvc.perform(put("/api/note/1")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(mapper.writeValueAsString(newNote)))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andExpect(content().string(containsString("New Test Note")));
+    }
 }
