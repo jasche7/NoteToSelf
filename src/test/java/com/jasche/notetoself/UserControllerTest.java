405a5bb00340a66ea3def1bb3a25786f8ccf1c89
Jasche7 <jasonc1172@gmail.com>
2021-07-08 17:00:23 -0700

    Removed unnecessary imports

--- src/test/java/com/jasche/notetoself/UserControllerTest.java
+++ src/test/java/com/jasche/notetoself/UserControllerTest.java
@@ -1,11 +1,9 @@
 package com.jasche.notetoself;
 
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.security.oauth2.core.oidc.OidcIdToken;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
