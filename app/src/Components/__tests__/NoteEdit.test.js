36d8a8d6e670ae491d8442bbee3722ab94507894
Jasche7 <jasonc1172@gmail.com>
2021-07-19 15:01:00 -0700

    Added testing for NoteEdit

---
+++ app/src/Components/__tests__/NoteEdit.test.js
@@ -0,0 +1,34 @@
+import NoteEdit from "../NoteEdit";
+import { render } from "@testing-library/react";
+import { StaticRouter } from "react-router-dom";
+
+describe(`The NoteEdit component`, () => {
+  it(`should have a form for inputting title and text for a Note and a button to submit the form`, () => {
+    const { getByLabelText, getByText } = render(
+      <StaticRouter>
+        <NoteEdit />
+      </StaticRouter>
+    );
+
+    expect(getByLabelText("Title").closest("Input")).toHaveAttribute(
+      "id",
+      "title"
+    );
+    expect(getByLabelText("Text").closest("Input")).toHaveAttribute(
+      "id",
+      "text"
+    );
+    expect(getByText("Save").closest("Button")).toHaveAttribute(
+      "type",
+      "submit"
+    );
+  });
+
+  it(`should take the user back to the Notes page after clicking cancel`, () => {
+    const { getByText } = render(
+      <StaticRouter>
+        <NoteEdit />
+      </StaticRouter>
+    );
+  });
+});
